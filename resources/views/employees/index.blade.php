2e3a527e215088d69441d61ecdbd64946d5e8b9d
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-10-05 18:37:29 +0200

    Created model, view folder, migrations and controllers for Task and Employees

---
+++ resources/views/employees/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+{{--<h1>Overzicht van alle collega's</h1>--}}
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/employees', App\Http\Controllers\EmployeeController::class);
+Route::resource('/tasks', App\Http\Controllers\TaskController::class);
