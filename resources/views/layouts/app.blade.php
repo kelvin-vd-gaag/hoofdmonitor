0e96cf0d23bb6d74a76d73e4cbe69bf0cea907ae
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-10-19 01:19:38 +0200

    Minor style issue fix icons margin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -264,10 +264,10 @@
                         aria-haspopup="true"
                     >
                         <span class="inline-flex items-center">
-                          <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                          <svg class="w-4 h-4" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                           <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                         </svg>
-                            <span class="">Account Beheer</span>
+                            <span class="ml-4">Account Beheer</span>
                         </span>
                         <svg
                             class="w-4 h-4"
