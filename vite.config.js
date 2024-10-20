410df2be807075b44100c1f6f63ee2ffee902ee0
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-10-20 17:33:14 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/tailwind.output.css'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
