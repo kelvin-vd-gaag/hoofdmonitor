a38c4145433d79fb5a2617bd938d53ffd357bf8a
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-10-21 16:12:21 +0200

    Test

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto px-4 py-8 mt-6">
-        <h2 class="text-2xl font-semibold text-gray-700 dark:text-gray-200 mb-6">Nieuwe Taak Aanmaken</h2>
+        <h2 class="text-2xl font-semibold text-gray-700 mb-6">Nieuwe Taak Aanmaken</h2>
 
         <!-- Formulier om een nieuwe taak aan te maken -->
         <form action="{{ route('tasks.store') }}" method="POST">
@@ -10,12 +10,12 @@
 
             <!-- Taaknaam -->
             <div class="mb-4">
-                <label for="name" class="block text-sm text-gray-700 dark:text-gray-400">Taaknaam</label>
+                <label for="name" class="block text-sm text-gray-700 ">Taaknaam</label>
                 <input
                     type="text"
                     name="name"
                     id="name"
-                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 form-input"
+                    class="block w-full mt-1 text-sm focus:border-purple-400 focus:outline-none focus:shadow-outline-purple form-input"
                     placeholder="Bijv. Ontwikkelen van nieuwe website"
                     required
                 />
@@ -26,12 +26,12 @@
 
             <!-- Beschrijving -->
             <div class="mb-4">
-                <label for="description" class="block text-sm text-gray-700 dark:text-gray-400">Beschrijving</label>
+                <label for="description" class="block text-sm text-gray-700">Beschrijving</label>
                 <textarea
                     name="description"
                     id="description"
                     rows="4"
-                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 form-textarea"
+                    class="block w-full mt-1 text-sm focus:border-purple-400 focus:outline-none focus:shadow-outline-purple form-textarea"
                     placeholder="Bijv. Details over de taak"
                     required
                 ></textarea>
@@ -42,12 +42,12 @@
 
             <!-- Taakuren -->
             <div class="mb-4">
-                <label for="hours" class="block text-sm text-gray-700 dark:text-gray-400">Toegewezen uren</label>
+                <label for="hours" class="block text-sm text-gray-700">Toegewezen uren</label>
                 <input
                     type="number"
                     name="hours"
                     id="hours"
-                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 form-input"
+                    class="block w-full mt-1 text-sm  focus:border-purple-400 focus:outline-none focus:shadow-outline-purple form-input"
                     placeholder="Aantal uren"
                     min="1"
                     required
@@ -59,12 +59,12 @@
 
             <!-- Startdatum -->
             <div class="mb-4">
-                <label for="start_date" class="block text-sm text-gray-700 dark:text-gray-400">Startdatum</label>
+                <label for="start_date" class="block text-sm text-gray-700">Startdatum</label>
                 <input
                     type="date"
                     name="start_date"
                     id="start_date"
-                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 form-input"
+                    class="block w-full mt-1 text-sm focus:border-purple-400 focus:outline-none focus:shadow-outline-purple form-input"
                     required
                 />
                 @error('start_date')
@@ -74,12 +74,12 @@
 
             <!-- Einddatum -->
             <div class="mb-4">
-                <label for="end_date" class="block text-sm text-gray-700 dark:text-gray-400">Einddatum</label>
+                <label for="end_date" class="block text-sm text-gray-700">Einddatum</label>
                 <input
                     type="date"
                     name="end_date"
                     id="end_date"
-                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 form-input"
+                    class="block w-full mt-1 text-sm focus:border-purple-400 focus:outline-none focus:shadow-outline-purple form-input"
                     required
                 />
                 @error('end_date')
@@ -91,7 +91,7 @@
             <div class="flex justify-end">
                 <button
                     type="submit"
-                    class="px-4 py-2 font-semibold text-white bg-purple-600 rounded-lg hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                    class="px-4 py-2 font-semibold text-white bg-limegreen rounded-lg hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
                 >
                     Taak Aanmaken
                 </button>
