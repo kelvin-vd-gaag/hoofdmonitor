7b91a1f801542d277c22d7f2f16e87b1543b096b
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-10-21 16:14:21 +0200

    Test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <h2
             class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
         >
-            Overzichtt
+            Overzicht
         </h2>
         {{--            <!-- CTA -->--}}
         {{--            <a--}}
