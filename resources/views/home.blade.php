8ea1ea6299dcf84cef9f0bcc9eb7521891a03860
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-10-22 13:20:34 +0200

    Voorbeeld

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <h2
             class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
         >
-            Overzicht
+            Voorbeeld
         </h2>
         {{--            <!-- CTA -->--}}
         {{--            <a--}}
