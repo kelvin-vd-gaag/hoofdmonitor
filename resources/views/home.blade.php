8634d7c7e79c60e39c8fed86085a307e8915e2e4
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-10-22 09:59:17 +0200

    Demonstratie terugzetten

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <h2
             class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
         >
-            Overzichtttttttttttt
+            Overzicht
         </h2>
         {{--            <!-- CTA -->--}}
         {{--            <a--}}
